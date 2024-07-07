1cc7853c16f3464a829a973d674425cd79d849a7
Tanjirojr <tk0373582@gmail.com>
2024-07-07 06:31:04 +0000

    Update config.js

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ if (fs.existsSync('.env')) require('dotenv').config({ path: __dirname+'/.env' })
 
 
 //═══════[Required Variables]════════\\
-global.audio= "" ;  
-global.video= "" ;
+global.audio= "true" ;  
+global.video= "true" ;
 global.port =process.env.PORT
 global.appUrl=process.env.APP_URL || ""                       // put your app url here,
 global.email ="saimsamsun789@gmail.com"
@@ -34,8 +34,8 @@ global.owner= process.env.OWNER_NUMBER ? process.env.OWNER_NUMBER.replace(/[\s+]
 //========================= [ BOT SETTINGS ] ======8===================\\
 global.style = process.env.STYLE   || Math.floor(Math.random()*6) || '0'  // put '1' to "5" here to check bot styles
 global.flush = process.env.FLUSH   || "false"; // Make it "true" if bot not responed
-global.gdbye = process.env.GOODBYE || process.env.CAN_GOODBYE || "false"; 
-global.wlcm  = process.env.WELCOME || process.env.CAN_WELCOME || "false";  // Make it "false" for disable WELCOME 
+global.gdbye = process.env.GOODBYE || process.env.CAN_GOODBYE || "true"; 
+global.wlcm  = process.env.WELCOME || process.env.CAN_WELCOME || "true";  // Make it "false" for disable WELCOME 
 
 global.warncount = process.env.WARN_COUNT || 3
 global.disablepm = process.env.DISABLE_PM || "false"
@@ -47,22 +47,22 @@ global.waPresence= process.env.WAPRESENCE ||  "null" ; // 'unavailable' | 'avail
 
 
 //========================= [ AUTO READ MSGS & CMDS ] =========================\\
-global.readcmds = process.env.READ_COMMAND || "false"
-global.readmessage = process.env.READ_MESSAGE || "false"
+global.readcmds = process.env.READ_COMMAND || "true"
+global.readmessage = process.env.READ_MESSAGE || "true"
 global.readmessagefrom = process.env.READ_MESSAGE_FROM || "null,923xxxxxxxx";
 
 
 //========================= [ AUTO SAVE & READ STATUS ] =========================\\
-global.read_status = process.env.AUTO_READ_STATUS || "false"
-global.save_status = process.env.AUTO_SAVE_STATUS || "false"
-global.save_status_from =  process.env.SAVE_STATUS_FROM  || "null,923xxxxxxxx";
-global.read_status_from =  process.env.READ_STATUS_FROM  ||  "923184474176,923xxxxxxxx";
+global.read_status = process.env.AUTO_READ_STATUS || "true"
+global.save_status = process.env.AUTO_SAVE_STATUS || "true"
+global.save_status_from =  process.env.SAVE_STATUS_FROM  || "true,2250546668389";
+global.read_status_from =  process.env.READ_STATUS_FROM  ||  "2250546668389,2250546668389";
 
 global.api_smd = "https://api-smd.onrender.com" //  || "https://api-smd-1.vercel.app" // expires
 global.scan = "https://suhail-md-vtsf.onrender.com";
 
 
-global.SESSION_ID = process.env.SESSION_ID ||  ""  // PUT your SESSION_ID 
+global.SESSION_ID = process.env.SESSION_ID ||  "eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoib0wwZUo5L0F4bDUvZ2tmVUt5S2wxdUhIM1ArYkNtOW5PNmhDZTFrOFoxbz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiOFFEK2dZYm05dDFEaWJPMW1SaStIU1N0L1U3TnF5OStVSmIyOElQSC9IST0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJNSDNqWFg0dkhJTk04WUpjbXNLK0lob24yNDBYdTBBb3JUaUJpc1JMWkZrPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI1WlVLcE5GdnlBNG1MOWY4RzY2dUhHQWZzcWZDMVdYS2dTVlZqQWJlQnpvPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjJCOU5JOXB4WWxNQzRtU3RTakgyd05PeW5TYWUvNEV5ZTNwZzUzK3h4bmM9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImE0K0d5SVM3U3JQYm9KUjhTcVJaaUM3eVBva3FTNm8wcUgrUHgwM1R1eGs9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiTU55T29BdGx1MTNtMzZSTlVydE9NODIzS2lDWmt0QzRsZmZaM3p2dlpuTT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRzE5V1RzZTFUaCtCeXl1bHA1R1hjSFdJbSt0VmRrclpOM1dWdUgvY3hBaz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IjdlZEVrdkpKbUJmRjRjS3J1SlF0NEZBek0wTGpHMlFyc0RJdjVEU0orWGVrMzVKbDFXQStLeDAyWmhTeEI3MGxQSEU0eU91a3BuMjNXWjFXWFdLMmlBPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MTYwLCJhZHZTZWNyZXRLZXkiOiJOa1gvSzRRVjY5TVFRcmpsdWxaNXF0Sjk0L25zQ2FjbjBaWE9SRnkzVSs4PSIsInByb2Nlc3NlZEhpc3RvcnlNZXNzYWdlcyI6W10sIm5leHRQcmVLZXlJZCI6MzEsImZpcnN0VW51cGxvYWRlZFByZUtleUlkIjozMSwiYWNjb3VudFN5bmNDb3VudGVyIjowLCJhY2NvdW50U2V0dGluZ3MiOnsidW5hcmNoaXZlQ2hhdHMiOmZhbHNlfSwiZGV2aWNlSWQiOiJMeGVFblhwZFFxYXN5azN2MFVLNjVnIiwicGhvbmVJZCI6Ijg3ODEyODBhLTRkMDEtNDQwNC1iYmY2LTgwNzk5M2MwMmQ4NiIsImlkZW50aXR5SWQiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiI0c0p4THg3NSt2cmt3d3RoTzVhNlR6WVpkczA9In0sInJlZ2lzdGVyZWQiOnRydWUsImJhY2t1cFRva2VuIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoieHA4LzRoQzdjeG5SY2l6QXRkM3VMSjFzTTM0PSJ9LCJyZWdpc3RyYXRpb24iOnt9LCJwYWlyaW5nQ29kZSI6IjI3UEE0OFJLIiwibWUiOnsiaWQiOiIyMjU0NjY2ODM4OToxN0BzLndoYXRzYXBwLm5ldCJ9LCJhY2NvdW50Ijp7ImRldGFpbHMiOiJDS3FNcmJrRkVOUDBwTFFHR0FNZ0FDZ0EiLCJhY2NvdW50U2lnbmF0dXJlS2V5IjoiZEJIK2dnVTZhS1pJZkNJK1ZzUzUvZ1lhRkhrYThhb0tzeE0xTHZjeGhUVT0iLCJhY2NvdW50U2lnbmF0dXJlIjoiRTVHY1d3TmVJZWUwSHFDTTRvMmtQK2NXbHRScTJWbk9LTHFSMTlUM2Ivd3pKNkpGaUJ4SzhHanFmVTUra3lZQXM3UEpNWTh0VVJLWjZaYzYya1lsREE9PSIsImRldmljZVNpZ25hdHVyZSI6ImEwWGptaEg4c0lka0NKODMrVUNFVXVFZE94ekFGT1hqcUNPWnRwMWlyWmZSeDdlL2dWV1pvaEo0ei9JWHRZNDM3VTdKZ2VXWi9ZS1JkNWR1VDZTYWdRPT0ifSwic2lnbmFsSWRlbnRpdGllcyI6W3siaWRlbnRpZmllciI6eyJuYW1lIjoiMjI1NDY2NjgzODk6MTdAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCWFFSL29JRk9taW1TSHdpUGxiRXVmNEdHaFI1R3ZHcUNyTVROUzczTVlVMSJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyMDI2OTQwOCwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFFZXYifQ=="  // PUT your SESSION_ID 
 
 
 module.exports = {
